477e08cb4dc01d437a889f694e5c5c4f0e6aae41
paulbouvignies <paul.bouvignies@pha5e.com>
2025-05-09 18:49:07 +0200

    Update video file paths in config.py to remove 'Desktop' directory

--- 01.DEV/config.py
+++ 01.DEV/config.py
@@ -19,20 +19,20 @@ DT_PIN = 20
 # Chemins des fichiers vidéo par langue
 VIDEOS = {
     "fr": {  # Français
-        "loop": "/home/pha5e/Desktop/24HLM/01.DEV/videos/fr/loop-video.mp4",
-        "once": "/home/pha5e/Desktop/24HLM/01.DEV/videos/fr/video-test.mp4"
+        "loop": "/home/pha5e/24HLM/01.DEV/videos/fr/loop-video.mp4",
+        "once": "/home/pha5e/24HLM/01.DEV/videos/fr/video-test.mp4"
     },
     "it": {  # Italien
-        "loop": "/home/pha5e/Desktop/24HLM/01.DEV/videos/it/loop-video.mp4",
-        "once": "/home/pha5e/Desktop/24HLM/01.DEV/videos/it/video-test.mp4"
+        "loop": "/home/pha5e/24HLM/01.DEV/videos/it/loop-video.mp4",
+        "once": "/home/pha5e/24HLM/01.DEV/videos/it/video-test.mp4"
     },
     "de": {  # Allemand
-        "loop": "/home/pha5e/Desktop/24HLM/01.DEV/videos/de/loop-video.mp4",
-        "once": "/home/pha5e/Desktop/24HLM/01.DEV/videos/de/video-test.mp4"
+        "loop": "/home/pha5e/24HLM/01.DEV/videos/de/loop-video.mp4",
+        "once": "/home/pha5e/24HLM/01.DEV/videos/de/video-test.mp4"
     },
     "en": {  # Anglais
-        "loop": "/home/pha5e/Desktop/24HLM/01.DEV/videos/en/loop-video.mp4",
-        "once": "/home/pha5e/Desktop/24HLM/01.DEV/videos/en/video-test.mp4"
+        "loop": "/home/pha5e/24HLM/01.DEV/videos/en/loop-video.mp4",
+        "once": "/home/pha5e/24HLM/01.DEV/videos/en/video-test.mp4"
     }
 }
 
